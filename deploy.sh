af904afcff17beb300f2c166d464bb12de204261
ychda <ychda@qq.com>
2022-03-31 07:18:25 +0800

    修改了deploy.sh文件

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #git remote add origin git@github.com:ychda/git-test.git
 git add --all
-git commit -m "从main分支增加一个文件"
+git commit -m "修改了deploy.sh文件"
+git push -u origin main
 git push -u origin master
 
 git status
