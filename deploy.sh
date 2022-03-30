3821c1d5a14cef7bc64b79044df9a0aaa560b191
ychda <ychda@qq.com>
2022-03-31 07:24:11 +0800

    合并分支后修改了deploy.sh文件

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #git remote add origin git@github.com:ychda/git-test.git
 git add --all
-git commit -m "修改了deploy.sh文件"
-git push -u origin main
+git commit -m "合并分支后修改了deploy.sh文件"
+#git push -u origin main
 git push -u origin master
 
 git status
 git branch -a
 git log --graph
 git checkout -b main
+
+
+#
+# git checkout master
+# git merge --no-ff main
+# git branch -d main
+#
